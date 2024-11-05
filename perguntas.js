e59dd94c6081102212cc31b946dbb5fc5f5a57a7
newton447 <00001093693290sp@al.educacao.sp.gov.br>
2024-11-05 10:52:56 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'filosofia',
+    'qual o sentido da vida?',
+    'não tem, voce faz o seu sentido'
+)
+
+criaCartao(
+    'filmes',
+    'qual o melhor filme de stop-motion?',
+    'coraline e o mundo secreto'
+)
+
+criaCartao(
+    'jogos',
+    'qual o nome do personagem verdinho que esplode no minedraft?',
+    'creeper'
+)
+
+criaCartao(
+    '.',
+    'qual o nome do animal que tem uma tromba?',
+    'elefante'
+)
